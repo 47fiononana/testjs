458045f48670eb721c6e548b9b11fe7d52d7472a
47fiononana <rantofiononanafanomezana@gmail.com>
2018-08-20 13:29:03 +0300

    nodejs

---
+++ test.js
@@ -0,0 +1,20 @@
+function my_max(array) {
+    console.log('le nombre maximum est:');
+    console.log(Math.max(...array));
+};
+let tab = [1,2,3,4,5]
+my_max(tab)
+
+function vowel_count(string) {
+    console.log('le nombre de vowel');
+    console.log(str.match(/[aeiou]/gi).length);
+   
+}
+var str = 'Hello world'
+vowel_count(str)
+function reverse(string) {
+    console.log('les mots inverser:')
+    console.log([...strr].reverse().join(''))
+}
+var strr = 'Hello world'
+reverse(strr)
